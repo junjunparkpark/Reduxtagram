b9d089f88173e911897374469f2d782bd9cde34f
Jun Park <jun.park@live.com>
2017-11-15 19:01:11 -0800

    Implement main component with header

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxtagram</Link>
+        </h1>
+      </div>
+    )
+  }
+})
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 
 // Import css
 import css from './styles/style.styl';
+import Main from './components/Main';
 
-render(<p>hi</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));
 
