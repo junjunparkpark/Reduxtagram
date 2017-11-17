976bd2df44c3c55bce251fd4f481a54fabd6c811
Jun Park <jun.park@live.com>
2017-11-16 22:36:18 -0800

    Create App component that maps state to props and maps dispatches to the props

---
+++ client/components/App.js
@@ -0,0 +1,21 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionsCreators';
+import Main from './Main';
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments,
+
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)
+// these 2 functions are going to
+
+export default App;
