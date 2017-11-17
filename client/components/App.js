e60f0a07cac348b94698d78c8a528039686dc11f
Jun Park <jun.park@live.com>
2017-11-16 22:38:44 -0800

    Pass down state and dispatch methods to Main component

--- client/components/App.js
+++ client/components/App.js
@@ -15,7 +15,8 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-const App = connect(mapStateToProps, mapDispatchToProps)
+const App = connect(mapStateToProps, mapDispatchToProps)(Main);
 // these 2 functions are going to
+// Immediately expressing Main will pass state and dispatches to Main component
 
 export default App;
