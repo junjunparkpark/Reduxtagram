ebe5347b1d39aea6398a34fc650701b34669c909
Jun Park <jun.park@live.com>
2017-11-16 20:22:45 -0800

    Create add and remove comment actions

--- client/actions/actionsCreator.js
+++ client/actions/actionsCreator.js
@@ -1,10 +1,25 @@
 // Increment
-function increment(index) {
+export function increment(index) {
   return {
     type: 'INCREMENT_LIKES',
     index: index
   }
 }
 // Add Comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
 
 // Remove Comment
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
