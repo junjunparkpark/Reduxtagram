38c622460feed7f6faeabd95f5c17b14f203f12d
Jun Park <jun.park@live.com>
2017-11-16 20:06:43 -0800

    Initialize Store for redux

---
+++ client/store.js
@@ -0,0 +1,7 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Import the root reducer
+import rootReducer from './reducers/index';
+
