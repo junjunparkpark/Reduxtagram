9c574639bc076c71ea83f34e10e8ec2d7f49b51d
Jun Park <jun.park@live.com>
2017-11-16 22:45:41 -0800

    Iterate over the posts in props in the PhotoGrid, create a Photo component for each post

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        
+        { this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post}/>)}
       </div>
     )
   }
