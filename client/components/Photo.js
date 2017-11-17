27977115901cdaa4942070e7536235626bcf9db5
Jun Park <jun.park@live.com>
2017-11-16 22:56:05 -0800

    Add image, likes and comment to each Photo component

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
-const Photo = () => (
+const Photo = ({ post, i, comments }) => (
   <figure className="grid-figure">
-    I am a photo
+    <div className="grid-photo-wrap">
+      <Link to={`/view/${post.code}`} >
+        <img src={post.display_src} alt={post.caption} className="grid-photo" />
+
+      </Link>
+      <CSSTransitionGroup transitionName="like"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
+        <span key={post.likes} className="likes-heart">{post.likes}</span>
+      </CSSTransitionGroup>
+    </div>
+
+    <figcaption>
+      <p>{post.caption}</p>
+      <div className="control-buttons">
+        <button className="likes">&hearts; {post.likes}</button>
+        </div>
+        <Link className="button" to={`/view/${post.code}`}>
+          <span className="comment-count">
+            <span className="speech-bubble">
+            </span>
+            {comments[post.code] ? comments[post.code].length : 0}
+          </span>
+        </Link>
+    </figcaption>
   </figure>
 );
 
