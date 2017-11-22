be9fdfb59a0bbd587ee55e4db50f5a1f6fe6d84f
Jun Park <jun.park@live.com>
2017-11-22 14:53:15 -0800

    Add onClick for incrementing likes, updates state for post likes

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
-const Photo = ({ post, i, comments }) => (
+const Photo = ({ post, i, comments, increment }) => (
   <figure className="grid-figure">
     <div className="grid-photo-wrap">
       <Link to={`/view/${post.code}`} >
@@ -19,12 +19,12 @@ const Photo = ({ post, i, comments }) => (
     <figcaption>
       <p>{post.caption}</p>
       <div className="control-buttons">
-        <button className="likes">&hearts; {post.likes}</button>
+        <button onClick={() => increment(i)} className="likes">&hearts; {post.likes}</button>
         <Link className="button" to={`/view/${post.code}`}>
           <span className="comment-count">
-            <span className="speech-bubble">
-            </span>
-            {comments[post.code] ? comments[post.code].length : 0}
+            <span className="speech-bubble"></span>
+
+            { comments[post.code] ? comments[post.code].length : 0}
           </span>
         </Link>
       </div>
