a22c73174a7f136da807ec8a8ece5d60d3576d19
Jun Park <jun.park@live.com>
2017-11-24 20:42:48 -0800

    Comments component renders each comment into its own div.

--- client/components/Single.js
+++ client/components/Single.js
@@ -8,10 +8,12 @@ class Single extends Component {
   render() {
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
     const post = this.props.posts[i];
+    const postComments = this.props.comments[this.props.params.postId] || [];
 
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
