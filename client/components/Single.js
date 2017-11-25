33b273cd015620f78cda32e859bcf4e9264c715c
Jun Park <jun.park@live.com>
2017-11-24 20:32:17 -0800

    Add Photo component with post props in Single Component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
+// import comments
 
 class Single extends Component {
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+    console.log(i);
+    // get us the post
+    const post = this.props.posts[i];
+    console.log(post);
+
+
     return (
       <div className="single-photo">
-
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
