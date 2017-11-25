a0a9daf53fc0bf64fd44b79382108057bd930825
Jun Park <jun.park@live.com>
2017-11-24 20:35:18 -0800

    Create Comment component, include it in the Single Component

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = () => {
+  return (
+    <div className="comment">
+      I'm a comment!
+    </div>
+  );
+}
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Photo from './Photo';
+import Comments from './Comments';
 // import comments
 
 class Single extends Component {
   render() {
-    // index of the post
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
-    console.log(i);
-    // get us the post
     const post = this.props.posts[i];
-    console.log(post);
-
 
     return (
       <div className="single-photo">
