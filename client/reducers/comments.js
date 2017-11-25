2ae33f3ff6616d8b489008392a6a3cda0f89be10
Jun Park <jun.park@live.com>
2017-11-24 21:28:31 -0800

    Use switch statement in the postComments function in the Comments reducers

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,16 @@
 function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return state;
+
+    default:
+      return state;
+  }
   return state;
 }
 
